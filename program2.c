2cad5ec46a536d22ed9b0b6a5f42d513de9e531a
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-07-26 04:18:06 -0700

    Create program2.c

---
+++ program2.c
@@ -0,0 +1,16 @@
+// Write a program to perform addition of 2 numbers
+
+#include<stdio.h>
+
+ int main()
+ {
+    int i = 10;
+    int j = 20;
+    int k = 0;
+
+    k = i + j;
+
+    printf("Addition is : %d\n",k);
+
+    return 0;
+ }
