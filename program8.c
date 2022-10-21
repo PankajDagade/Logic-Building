7220b82d11c8ad84038fdc1cc2629a325e9114f6
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-10-21 08:50:20 -0700

    Create program8.c

---
+++ program8.c
@@ -0,0 +1,23 @@
+// Program to display 5 times Hello on screen
+
+#include<stdio.h>
+
+// Demonstration of SEQUANCE
+
+void Display();                 // Declaration
+
+int main()
+{
+    Display();                  // Function call
+    
+    return 0;
+}
+
+void Display()              // Defination
+{
+    printf("Hello\n");
+    printf("Hello\n");
+    printf("Hello\n");
+    printf("Hello\n");
+    printf("Hello\n");
+}
