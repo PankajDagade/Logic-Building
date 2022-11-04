80bdf73095edb1b8eb5d6ba6586ba7508e78cb5f
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-11-03 21:33:32 -0700

    Create program33.c

---
+++ program33.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+////////////////////////////////////////////////////////////////
+//////////////////////////////////////
+//
+//Function Name:     DisplayDigits
+//Description  :     Seperate digits from whole number
+//Input        :     6587
+//Output       :     7856
+//Date         :     29/04/2022
+//Author       :     Pankaj Vishnu Dagade
+//
+////////////////////////////////////////////////////////////////
+//////////////////////////////////////
+
+void DisplayDigits(int iNo)
+{
+    int iDigit = 0;
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        printf("%d\n",iDigit);
+        iNo = iNo /10;
+    }
+}
+///////////////////////////////////////////////////////////////
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+
+    DisplayDigits(iValue);
+
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////
+//
+//Input - Enter number : 6587
+//Output - 7856
+//
+////////////////////////////////////////////////////////////////
