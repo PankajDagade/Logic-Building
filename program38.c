17bbc53d87b1665cff17ffdc60f60e2e0f07ac96
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-11-03 21:51:31 -0700

    Create program38.c

---
+++ program38.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+////////////////////////////////////////////////////////////////
+//////////////////////////////////////
+//
+//Function Name:     Reverse
+//Description  :     Reverse number from whole number
+//Input        :     123456
+//Output       :     12
+//Date         :     29/04/2022
+//Author       :     Pankaj Vishnu Dagade
+//
+////////////////////////////////////////////////////////////////
+//////////////////////////////////////
+
+int Reverse(int iNo)
+{
+    int iDigit = 0;
+    int iRev = 0;
+
+    while(iNo > 0)
+    {
+        iDigit = iNo % 10;
+        iRev = iRev * 10 + iDigit;
+        iNo = iNo / 10;
+    }
+
+   return iRev;
+}
+////////////////////////////////////////////////////////////////
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = Reverse(iValue); // Reverse(7562)
+
+    printf("Reverse number is : %d\n",iRet);
+
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////
+//
+//Input - Enter number : 123456
+//Output - 654321
+//
+////////////////////////////////////////////////////////////////
