fb7af39183cc23415777b8bc6c7a81cce94acb40
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-11-03 20:10:58 -0700

    Create program10.c

---
+++ program10.c
@@ -0,0 +1,20 @@
+// Program to display 1 to 5 on screen
+// Output   1   2   3   4   5
+
+#include<stdio.h>
+
+void Display()
+{
+    printf("1\n");
+    printf("2\n");
+    printf("3\n");
+    printf("4\n");
+    printf("5\n");
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
