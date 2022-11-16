0277e0c65e2f397d96037e10d9398a7e367cadb1
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-11-16 07:11:49 -0800

    Create program69.c

---
+++ program69.c
@@ -0,0 +1,50 @@
+// Input :  Row 4   Columns     4
+/*
+$   2   3   4
+1   $   3   4
+1   2   $   4
+1   2   3   $
+*/
+
+#include<stdio.h>
+
+void Display(int iRow, int iCol)
+{
+        int i = 0, j = 0;
+        if(iRow != iCol)
+        {
+            printf("Invalid input\n");
+            return;
+        }
+
+        for(i = 1; i <= iRow;i++)
+        {
+            for(j = 1; j<= iCol; j++)
+            {
+                    if(i == j)
+                    {
+                            printf("$\t");
+                    }
+                    else
+                    {
+                            printf("%d\t",j);
+                    }
+            }
+            printf("\n");
+        }
+}
+
+int main()
+{
+    int iValue1 =0, iValue2 = 0;
+
+    printf("Enter number of rows :\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns :\n");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+
+    return 0;
+}
