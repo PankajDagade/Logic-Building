22d3fbdf3e9846ab97ddccf11a73e07c68f6d296
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-11-16 06:55:03 -0800

    Update program55.c

--- program55.c
+++ program55.c
@@ -1,53 +1,82 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<stdbool.h>
 
-bool Search(int Arr[], int iLength, int iNo)
+////////////////////////////////////////////////////////////////
+//////////////////////////////////////
+//
+//Function Name:     Searching
+//Description  :     Display Searching number from Array Elements
+//Input        :     ---
+//Output       :     ---
+//Date         :     29/04/2022
+//Author       :     Pankaj Vishnu Dagade
+//
+////////////////////////////////////////////////////////////////
+//////////////////////////////////////
+
+
+int Searching(int Arr[], int iLength , int iNo)
 {
     int iCnt = 0;
-    bool bFlag = false;
-    //          1               2               3
-    for(iCnt = 0; iCnt < iLength; iCnt++)
-    {
-            if(Arr[iCnt] == iNo)        // 4
-            {
-                bFlag = true;
-                break;
-            }
-    }
-    return bFlag;
-}
 
+    for(iCnt = 0 ; iCnt < iLength ; iCnt++)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            break;
+        }
+    }
+    if(iCnt == iLength)
+    {
+        return -1;
+    }
+    else
+    {
+        return iCnt;
+    }
+
+}
+////////////////////////////////////////////////////////////////
 int main()
 {
-    int iSize = 0, iCnt = 0, iValue = 0;
-    bool bRet;
-    int *ptr = NULL;
+    auto int *ptr = NULL;
+    auto int iSize = 0, iCnt = 0, iValue = 0 , iRet = 0;
 
-    printf("Enter number of elements\n");
+    printf("Enter the number of Elements :");
     scanf("%d",&iSize);
 
-    ptr = (int *)malloc(sizeof(int) * iSize);
+    ptr = (int *)malloc(iSize * sizeof(int));
 
-    printf("Enter the values\n");
-    for(iCnt = 0; iCnt < iSize; iCnt++)
+    printf("Enter the Elements : \n");
+    for(iCnt = 0 ; iCnt < iSize ; iCnt++)
     {
         scanf("%d",&ptr[iCnt]);
     }
 
-    printf("Enter the element to search\n");
+    printf("Enter the number that you want to search :");
     scanf("%d",&iValue);
 
-    bRet = Search(ptr, iSize, iValue);
-    if(bRet == true)
+    iRet = Searching(ptr,iSize,iValue);
+
+    if(iRet == -1)
     {
-        printf("Element is there in array\n");
+        printf("There is no such Element in Array");
     }
     else
     {
-        printf("Element is not there in array\n");
+        printf("Element first occured at : %d ",iRet);
     }
-     free(ptr);
-
     return 0;
 }
+
+////////////////////////////////////////////////////////////////
+//
+//Input - Enter the number of Elements :
+//         2
+//        Enter the Elements :
+//        10
+//        20
+//       Enter the number that you want to search :10
+//Output -Element first occured at : 0
+//        
+////////////////////////////////////////////////////////////////
