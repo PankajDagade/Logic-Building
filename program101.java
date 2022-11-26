9037d525dbdab105890c0bfb79da401cc959fffa
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-11-25 23:54:09 -0800

    Update program101.java

--- program101.java
+++ program101.java
@@ -18,32 +18,32 @@ class Pattern
 	{
 		      int iRow = ((iCol/2)+1);
               for(int i = 1 ; i <= (iRow-1) ; i++)
-			  {
-				  for(int j = 1 ; j <= iCol ; j++)
-				  {
-					  if(j == ((iCol/2)+1))
-					  {
-						  System.out.print("* ");
-					  }else{
-						  System.out.print("  ");
-					  }
-				  }
-				  System.out.println();
-			  }
-			  for(int i = 1 ; i <= iRow ; i++)
-			  {
-				  for(int j = 1 ; j <= iCol ; j++)
-				  {
-					  if((i == 1) || (j == 1) || (j == iCol) || (i == iRow))
-					  {
-						  System.out.print("* ");
-					  }else{
-						  System.out.print("  ");
-					  }
-				  }
-				  System.out.println();
-			  }
-	}			 
+	      {
+		      for(int j = 1 ; j <= iCol ; j++)
+		      {
+			     if(j == ((iCol/2)+1))
+			     {
+				   System.out.print("* ");
+			      }else{
+				   System.out.print("  ");
+			      }
+			}
+			System.out.println();
+		}
+		for(int i = 1 ; i <= iRow ; i++)
+		{
+			for(int j = 1 ; j <= iCol ; j++)
+			{
+				f((i == 1) || (j == 1) || (j == iCol) || (i == iRow))
+			        {
+				        System.out.print("* ");
+				 }else{
+					System.out.print("  ");
+				 }
+			}
+			System.out.println();
+		}
+	 }			 
 }
 
 class program101
